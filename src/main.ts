f2aa23aa6b62a0fcce9772b1b6f70efbfaa88767
ilyass mar <ilyass.marghine22@gmail.com>
2026-01-21 11:56:13 +0100

    first commit where i  created the angular project, and changed the main.ts and the costumized it to show a title including a data binded from the app class in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { Component } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+@Component({
+  selector: "app-root",
+  standalone: true,
+  template: `<h2> Hello Mr {{name}}</h2>`
+})
+
+class App{
+
+  name: string = "ilyass";
+
+}
+
+bootstrapApplication(App)
